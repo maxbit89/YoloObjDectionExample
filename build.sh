1bb6dfc53f448fec5d0208a6e4be8bc1e4e27134
maxbit89 <maxbit89@gmail.com>
2020-05-22 22:06:50 +0200

    forked darkflow to fix errors

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #https://pjreddie.com/darknet/yolov2/
 mkdir data
 cd data && wget -N https://pjreddie.com/media/files/yolov2.weights
+wget -N https://drive.google.com/drive/folders/1jKKb0e5kocPtBpEeg9UhPLhnxmTsBSTy?usp=sharing
 cd ..
 cd darkflow && git pull && git checkout master 
 cd ..
